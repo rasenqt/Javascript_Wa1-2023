77f2cd08164abc1695caa6bc63827719ab35c847
rasenqt <mikicaretto@gmail.com>
2023-03-16 20:35:40 +0100

    wee

--- Week3/db.js
+++ Week3/db.js
@@ -1,21 +1,85 @@
-"use strict";
-const sqlite=require('sqlite3');
+'use strict';
 
-const db=new sqlite.Database('questions.sqlite',(err)=>  {if(err)   throw err;  } );
- //parmaeter =? e poi metto nei parmatri (sql,[parmametri]) 
-function authorsofQuestionNUmber(qestNo)
-{  
-    return newPromise((resolve,reject)=> {
-    const sql= 'SELECT author FROM ANSWER WHERE questionId= ?'; 
-db.all(sql,[qestNo], (err,rows)=>{  
-    if (err)   throw err
-         else{ 
-         const  authorList=rows.map((x)=>x['author']);
-        resolve(authorList);
-         }
-           });
-        })
+const dayjs = require('dayjs');
+const sqlite = require('sqlite3');
+
+const db = new sqlite.Database('questions.sqlite', (err) => {
+    if (err) throw err;
+});
+
+function QuestionList() {
+
+    this.getQuestion = function (id) {
+
+        return new Promise((resolve, reject) => {
+            const sql = 'SELECT * FROM question WHERE id = ?';
+            db.get(sql, [id], (err, row) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(new Question(row.id, row.text, row.author, row.date));
+                }
+            });
+        });
     }
-const myAuthors= authorsofQuestionNUmber(1);
-myAuthors.then((list)=> {console.log(list)});
 
+    this.addQuestion = function(question) {
+        return new Promise((resolve, reject)=> {
+            const sql = 'INSERT INTO question(id, text, author, date) VALUES(?,?,?,?)' ;
+            db.run(sql, [question.id, question.text, question.author, question.date.toISOString()], (err)=>{
+                if(err)
+                    reject(err);
+                else
+                    resolve(true);
+            }) ;
+
+        });
+    }
+
+}
+
+function Answer(id, text, author, score, date) {
+    this.id = id;
+    this.text = text;
+    this.author = author;
+    this.score = score;
+    this.date = dayjs(date);
+}
+
+function Question(id, text, author, date) {
+    this.id = id;
+    this.text = text;
+    this.author = author;
+    this.date = dayjs(date);
+
+    this.getAnswers = function() {
+        return new Promise((resolve, reject)=>{
+            const sql = 'SELECT * FROM answer WHERE questionId = ?' ;
+            db.all(sql, [this.id], (err,rows)=>{
+                if(err)
+                    reject(err)
+                else {
+                    const answers = rows.map((a)=>new Answer(a.id, a.text, a.author, a. score, a.date)) ;
+                    resolve(answers) ;
+                }
+            });
+        }) ;
+    }
+}
+
+const ql = new QuestionList();
+
+ql.getQuestion(1).then((question)=>{
+    console.log(question);
+    question.getAnswers().then((answers)=>{console.log(answers)}) ;
+})
+
+ql.getQuestion(2)
+    .then(question=>question.getAnswers())
+    .then(answers=>console.log(`We have ${answers.length} anwsers`)) ;
+
+ql.addQuestion(new Question(3, 'How are you?', 'Fulvio', '2023-03-14'));
+
+
+
+debugger;
